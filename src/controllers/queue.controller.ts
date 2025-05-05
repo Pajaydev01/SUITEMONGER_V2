f11d88d7bbd8650175d4b08f62a50307b44ab896
pajaydev01 <johnsonaniyi1@gmail.com>
2025-05-05 13:33:41 +0100

    updated

--- src/controllers/queue.controller.ts
+++ src/controllers/queue.controller.ts
@@ -32,7 +32,7 @@ class QueueController {
         }
     }
 
-    public static async RemoveJob(job:Queue.Job, done:Queue.DoneCallback){
+    public static async RemoveJob(job: Queue.Job, done: Queue.DoneCallback) {
         try {
             const itemDel = job.data.items;
             const deler = await cacher.deleteItem(itemDel.name);
@@ -54,59 +54,63 @@ class QueueController {
             console.error('Error processing email job:', error);
             await job.releaseLock();
             job.remove()
-            done();   
+            done();
         }
     }
 
     public static async DetectFaceJob(job: Queue.Job, done: Queue.DoneCallback) {
         try {
-            const item= job.data.items;
+            const item = job.data.items;
             const checker = await actionService.DetectFaceMesh(item.photo);
             //do the checks and save to the database
-            
+
             if (checker) {
                 console.log('face detected', checker);
-                if(checker.score<0.7){
-                    const kc=await kyc.findOne({where:{id:item.kyc_id}});
-                    const user=await users.findOne({where:{id:kc.dataValues.user_id}});
+                console.log('item returned', item)
+                if (checker.score < 0.7) {
+                    const kc = await kyc.findOne({ where: { id: item.kyc_id } });
+                    console.log('here 1', kc.dataValues)
+                    const user = await users.findOne({ where: { id: kc.dataValues.user_id } });
                     await kc.update({
-                        facial_photo_status:'NO FACE DETECTED',
-                        status:'REJECTED',
-                        reason:'NO FACE DETECTED'
+                        facial_photo_status: 'NO FACE DETECTED',
+                        status: 'REJECTED',
+                        reason: 'NO FACE DETECTED'
                     });
                     //send email to the user
-                    await notificationService.sendMail(user.dataValues.email,'Face detection failed','mail',{name:user.dataValues.name,body:`Dear ${user.dataValues.first_name}, your facial photo was not detected, please try again`,email:user.dataValues.email});
+                    await notificationService.sendMail(user.dataValues.email, 'Face detection failed', 'mail', { name: user.dataValues.name, body: `Dear ${user.dataValues.first_name}, your facial photo was not detected, please try again`, email: user.dataValues.email });
 
-                    await socketIo.SendMessage(JSON.stringify({type:'error',message:'Face not detected'}),user,'face_detection');
-                }else{
-                    const kc=await kyc.findOne({where:{id:item.kyc_id}});
-                    const user=await users.findOne({where:{id:kc.dataValues.user_id}});
+                    await socketIo.SendMessage(JSON.stringify({ type: 'error', message: 'Face not detected' }), user, 'face_detection');
+                } else {
+                    const kc = await kyc.findOne({ where: { id: item.kyc_id } });
+                    console.log('here', kc.dataValues)
+                    const user = await users.findOne({ where: { id: kc.dataValues.user_id } });
                     //update the kyc status to verified
                     await kc.update({
-                        facial_photo_status:'FACE DETECTED',
-                        status:'VERIFIED',
-                        reason:'FACE DETECTED'
+                        facial_photo_status: 'FACE DETECTED',
+                        status: 'VERIFIED',
+                        reason: 'FACE DETECTED'
                     });
                     //update the user status to active
                     await user.update({
-                        status:userStatus.USER_STATUS_ACTIVE
+                        status: userStatus.USER_STATUS_ACTIVE
                     });
                     //send email to the user
-                    await notificationService.sendMail(user.dataValues.email,'Face detection successful','mail',{name:user.dataValues.first_name,body:`Dear ${user.dataValues.first_name}, your facial photo was detected successfully`,email:user.dataValues.email});
-                    await socketIo.SendMessage(JSON.stringify({type:'success',message:'FACE DETECTED'}),user,'face_detection');
+                    await notificationService.sendMail(user.dataValues.email, 'Face detection successful', 'mail', { name: user.dataValues.first_name, body: `Dear ${user.dataValues.first_name}, your facial photo was detected successfully`, email: user.dataValues.email });
+                    await socketIo.SendMessage(JSON.stringify({ type: 'success', message: 'FACE DETECTED' }), user, 'face_detection');
                 }
             } else {
                 console.log('error detecting face', checker);
-                const kc=await kyc.findOne({where:{id:item.kyc_id}});
-                const user=await users.findOne({where:{id:kc.dataValues.user_id}});
+                console.log('item3', item)
+                const kc = await kyc.findOne({ where: { id: item.kyc_id } });
+                const user = await users.findOne({ where: { id: kc.dataValues.user_id } });
                 await kc.update({
-                    facial_photo_status:'NO FACE DETECTED',
-                    status:'REJECTED',
-                    reason:'NO FACE DETECTED'
+                    facial_photo_status: 'NO FACE DETECTED',
+                    status: 'REJECTED',
+                    reason: 'NO FACE DETECTED'
                 });
                 //send email to the user
-                await notificationService.sendMail(user.dataValues.email,'Face detection failed','mail',{name:user.dataValues.name,body:`Dear ${user.dataValues.name}, your facial photo was not detected, please try again`,email:user.dataValues.email}); 
-                await socketIo.SendMessage(JSON.stringify({type:'error',message:'No face detected'}),user,'face_detection');
+                await notificationService.sendMail(user.dataValues.email, 'Face detection failed', 'mail', { name: user.dataValues.name, body: `Dear ${user.dataValues.name}, your facial photo was not detected, please try again`, email: user.dataValues.email });
+                await socketIo.SendMessage(JSON.stringify({ type: 'error', message: 'No face detected' }), user, 'face_detection');
             }
             await job.releaseLock();
             job.remove()
--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -8,142 +8,143 @@ import otps, { otpsType } from "../database/models/otps.model";
 import kyc from "../database/models/kyc.model";
 
 class userController {
-    public static CreateUser=async (req:Request, res: Response)=>{
+    public static CreateUser = async (req: Request, res: Response) => {
         try {
-            const body= actionService.getBody(req);
-            const checker=await users.findOne({where:{email:body.email}});
-            if(checker) return responseService.respond(res,{},412,false,'User already exists');
+            const body = actionService.getBody(req);
+            const checker = await users.findOne({ where: { email: body.email } });
+            if (checker) return responseService.respond(res, {}, 412, false, 'User already exists');
             //salt and hash the password
-            const password=await actionService.hasher(body.password);
-            body.password=password.hash;
-            body.salt=password.salt;
+            const password = await actionService.hasher(body.password);
+            body.password = password.hash;
+            body.salt = password.salt;
             //create the user
-            const user=await users.create(body);
+            const user = await users.create(body);
             //get the user token
             //const token=await AuthService.generateUserToken(user);
-            const otp=actionService.genOTP(4);
+            const otp = actionService.genOTP(4);
             //save in otps  
             await otps.create({
-                user_id:user.dataValues.id,
+                user_id: user.dataValues.id,
                 otp,
-                type:otpsType.OTP_TYPE_SIGNUP
+                type: otpsType.OTP_TYPE_SIGNUP
             });
             //send email
-            await notificationService.sendMail(user.dataValues.email,'Welcome to SuiteMonger','mail',{name:user.dataValues.first_name,body:`Dear ${user.dataValues.name}, welcome to SuiteMonger. Your account has been created successfully, use ${otp} to verify your account`,email:user.dataValues.email});
+            await notificationService.sendMail(user.dataValues.email, 'Welcome to SuiteMonger', 'mail', { name: user.dataValues.first_name, body: `Dear ${user.dataValues.name}, welcome to SuiteMonger. Your account has been created successfully, use ${otp} to verify your account`, email: user.dataValues.email });
             delete user.dataValues.password;
             delete user.dataValues.salt;
-            return responseService.respond(res,user,201,true,'User created');
+            return responseService.respond(res, user, 201, true, 'User created');
         } catch (error) {
-                        responseService.respond(res, error.data ? error.data : error, error.code && typeof error.code == 'number' ? error.code : 500, false, error.message ? error.message : 'Server error');
+            responseService.respond(res, error.data ? error.data : error, error.code && typeof error.code == 'number' ? error.code : 500, false, error.message ? error.message : 'Server error');
         }
     }
 
-    public static Login=async (req:Request, res: Response)=>{
+    public static Login = async (req: Request, res: Response) => {
         try {
-            const body=actionService.getBody(req);
-            const user=await users.scope('withPassword').findOne({where:{email:body.email}});
-            if(!user) return responseService.respond(res,{},401,false,'Invalid email');
+            const body = actionService.getBody(req);
+            const user = await users.scope('withPassword').findOne({ where: { email: body.email } });
+            if (!user) return responseService.respond(res, {}, 401, false, 'Invalid email');
             //check password
-            const check=await actionService.compare(user.dataValues.password,body.password,user.dataValues.salt);
-            if(!check) return responseService.respond(res,{},401,false,'Invalid email or password');
+            const check = await actionService.compare(user.dataValues.password, body.password, user.dataValues.salt);
+            if (!check) return responseService.respond(res, {}, 401, false, 'Invalid email or password');
             //get the user token
-            const token=await AuthService.generateUserToken(user);
+            const token = await AuthService.generateUserToken(user);
             delete user.dataValues.password;
             delete user.dataValues.salt;
-            return responseService.respond(res,{user},200,true,'User logged in',token);
+            return responseService.respond(res, { user }, 200, true, 'User logged in', token);
         } catch (error) {
-                        responseService.respond(res, error.data ? error.data : error, error.code && typeof error.code == 'number' ? error.code : 500, false, error.message ? error.message : 'Server error');
+            responseService.respond(res, error.data ? error.data : error, error.code && typeof error.code == 'number' ? error.code : 500, false, error.message ? error.message : 'Server error');
         }
     }
 
 
-    public static GetUser=async (req:Request, res: Response)=>{
+    public static GetUser = async (req: Request, res: Response) => {
         try {
-            let user=await AuthService.GetAuthUser();
-            user=await users.findOne({where:{id:user.dataValues.id},include:[{model:kyc,as:'kyc'}]});
-            return responseService.respond(res,user,200,true,'User found');
+            let user = await AuthService.GetAuthUser();
+            user = await users.findOne({ where: { id: user.dataValues.id }, include: [{ model: kyc, as: 'kyc' }] });
+            return responseService.respond(res, user, 200, true, 'User found');
         } catch (error) {
-                        responseService.respond(res, error.data ? error.data : error, error.code && typeof error.code == 'number' ? error.code : 500, false, error.message ? error.message : 'Server error');
+            responseService.respond(res, error.data ? error.data : error, error.code && typeof error.code == 'number' ? error.code : 500, false, error.message ? error.message : 'Server error');
         }
     }
 
-    public static VerifyUser=async (req:Request, res: Response)=>{
+    public static VerifyUser = async (req: Request, res: Response) => {
         try {
-            const body=actionService.getBody(req);
-            let user=await users.findOne({where:{email:body.email}});
-            const checker=await otps.findOne({where:{user_id:user.dataValues.id,otp:body.otp,type:otpsType.OTP_TYPE_SIGNUP}});
-            if(!checker) return responseService.respond(res,{},401,false,'Invalid OTP or email');
+            const body = actionService.getBody(req);
+            let user = await users.findOne({ where: { email: body.email } });
+            if (!user) return responseService.respond(res, {}, 412, false, 'Invalid email')
+            const checker = await otps.findOne({ where: { user_id: user.dataValues.id, otp: body.otp, type: otpsType.OTP_TYPE_SIGNUP } });
+            if (!checker) return responseService.respond(res, {}, 401, false, 'Invalid OTP or email');
             //check time diff for expiry
-            const timeDiff=actionService.getTimeDiff(checker.dataValues.createdAt);
-            console.log('time diff',timeDiff);
-            if(timeDiff>10) return responseService.respond(res,{},401,false,'OTP expired');
-            await users.update({status:userStatus.USER_STATUS_PENDING},{where:{id:user.dataValues.id}});
-            user=await users.findOne({where:{id:user.dataValues.id}});
-            await otps.destroy({where:{user_id:user.dataValues.id}});
-            return responseService.respond(res,user,200,true,'User verified');
+            const timeDiff = actionService.getTimeDiff(checker.dataValues.createdAt);
+            console.log('time diff', timeDiff);
+            if (timeDiff > 10) return responseService.respond(res, {}, 401, false, 'OTP expired');
+            await users.update({ status: userStatus.USER_STATUS_PENDING }, { where: { id: user.dataValues.id } });
+            user = await users.findOne({ where: { id: user.dataValues.id } });
+            await otps.destroy({ where: { user_id: user.dataValues.id } });
+            return responseService.respond(res, user, 200, true, 'User verified');
         } catch (error) {
-                        responseService.respond(res, error.data ? error.data : error, error.code && typeof error.code == 'number' ? error.code : 500, false, error.message ? error.message : 'Server error');
+            responseService.respond(res, error.data ? error.data : error, error.code && typeof error.code == 'number' ? error.code : 500, false, error.message ? error.message : 'Server error');
         }
     }
 
-    public static ResendOTP=async (req:Request, res: Response)=>{
+    public static ResendOTP = async (req: Request, res: Response) => {
         try {
-            const body=actionService.getBody(req);
-            let user=await users.findOne({where:{email:body.email}});
-            if(!user) return responseService.respond(res,{},401,false,'Invalid email');
-            const otp=actionService.genOTP(4);
+            const body = actionService.getBody(req);
+            let user = await users.findOne({ where: { email: body.email } });
+            if (!user) return responseService.respond(res, {}, 401, false, 'Invalid email');
+            const otp = actionService.genOTP(4);
             await otps.create({
-                user_id:user.dataValues.id,
+                user_id: user.dataValues.id,
                 otp,
-                type:body.type=='password'?otpsType.OTP_TYPE_FORGOT_PASS:otpsType.OTP_TYPE_SIGNUP
+                type: body.type == 'password' ? otpsType.OTP_TYPE_FORGOT_PASS : otpsType.OTP_TYPE_SIGNUP
             });
             //send email
-            await notificationService.sendMail(user.dataValues.email,body.type=='password'?'Password reset':'Welcome to SuiteMonger','mail',{name:user.dataValues.first_name,body:body.type=='password'?`Dear ${user.dataValues.first_name},use ${otp} to reset your password`:`Dear ${user.dataValues.first_name}, welcome to SuiteMonger. Your account has been created successfully, use ${otp} to verify your account`,email:user.dataValues.email});
-            return responseService.respond(res,user,200,true,'OTP resent');
+            await notificationService.sendMail(user.dataValues.email, body.type == 'password' ? 'Password reset' : 'Welcome to SuiteMonger', 'mail', { name: user.dataValues.first_name, body: body.type == 'password' ? `Dear ${user.dataValues.first_name},use ${otp} to reset your password` : `Dear ${user.dataValues.first_name}, welcome to SuiteMonger. Your account has been created successfully, use ${otp} to verify your account`, email: user.dataValues.email });
+            return responseService.respond(res, user, 200, true, 'OTP resent');
         } catch (error) {
-                        responseService.respond(res, error.data ? error.data : error, error.code && typeof error.code == 'number' ? error.code : 500, false, error.message ? error.message : 'Server error');
+            responseService.respond(res, error.data ? error.data : error, error.code && typeof error.code == 'number' ? error.code : 500, false, error.message ? error.message : 'Server error');
         }
     }
 
-    public static ForgotPassword=async (req:Request, res: Response)=>{
+    public static ForgotPassword = async (req: Request, res: Response) => {
         try {
-            const body=actionService.getBody(req);
-            let user=await users.findOne({where:{email:body.email}});
-            if(!user) return responseService.respond(res,{},401,false,'Invalid email');
-            const otp=actionService.genOTP(4);
+            const body = actionService.getBody(req);
+            let user = await users.findOne({ where: { email: body.email } });
+            if (!user) return responseService.respond(res, {}, 401, false, 'Invalid email');
+            const otp = actionService.genOTP(4);
             await otps.create({
-                user_id:user.dataValues.id,
+                user_id: user.dataValues.id,
                 otp,
-                type:otpsType.OTP_TYPE_FORGOT_PASS
+                type: otpsType.OTP_TYPE_FORGOT_PASS
             });
             //send email
-            await notificationService.sendMail(user.dataValues.email,'Password reset','mail',{name:user.dataValues.name,body:`Dear ${user.dataValues.first_name}, Kindly use ${otp} to reset your account password`,email:user.dataValues.email});
-            return responseService.respond(res,{},200,true,'OTP sent');
+            await notificationService.sendMail(user.dataValues.email, 'Password reset', 'mail', { name: user.dataValues.name, body: `Dear ${user.dataValues.first_name}, Kindly use ${otp} to reset your account password`, email: user.dataValues.email });
+            return responseService.respond(res, {}, 200, true, 'OTP sent');
         } catch (error) {
-                        responseService.respond(res, error.data ? error.data : error, error.code && typeof error.code == 'number' ? error.code : 500, false, error.message ? error.message : 'Server error');
+            responseService.respond(res, error.data ? error.data : error, error.code && typeof error.code == 'number' ? error.code : 500, false, error.message ? error.message : 'Server error');
         }
     }
 
-    public static ResetPassword=async (req:Request, res: Response)=>{
+    public static ResetPassword = async (req: Request, res: Response) => {
         try {
-            const body=actionService.getBody(req);
-            let user=await users.findOne({where:{email:body.email}});
-            if(!user) return responseService.respond(res,{},401,false,'Invalid email');
-            const checker=await otps.findOne({where:{user_id:user.dataValues.id,otp:body.otp,type:otpsType.OTP_TYPE_FORGOT_PASS}});
-            if(!checker) return responseService.respond(res,{},401,false,'Invalid OTP or email');
+            const body = actionService.getBody(req);
+            let user = await users.findOne({ where: { email: body.email } });
+            if (!user) return responseService.respond(res, {}, 401, false, 'Invalid email');
+            const checker = await otps.findOne({ where: { user_id: user.dataValues.id, otp: body.otp, type: otpsType.OTP_TYPE_FORGOT_PASS } });
+            if (!checker) return responseService.respond(res, {}, 401, false, 'Invalid OTP or email');
             //check time diff for expiry
-            const timeDiff=actionService.getTimeDiff(checker.dataValues.createdAt);
+            const timeDiff = actionService.getTimeDiff(checker.dataValues.createdAt);
 
-            if(timeDiff>10) return responseService.respond(res,{},401,false,'OTP expired');
+            if (timeDiff > 10) return responseService.respond(res, {}, 401, false, 'OTP expired');
             //salt and hash the password
-            const password=await actionService.hasher(body.password);
-            body.password=password.hash;
-            body.salt=password.salt;
-            await users.update({password:body.password,salt:body.salt},{where:{id:user.dataValues.id}});
-            await otps.destroy({where:{user_id:user.dataValues.id,type:'FORGOT_PASS'}});
-            return responseService.respond(res,{},200,true,'Password reset');
+            const password = await actionService.hasher(body.password);
+            body.password = password.hash;
+            body.salt = password.salt;
+            await users.update({ password: body.password, salt: body.salt }, { where: { id: user.dataValues.id } });
+            await otps.destroy({ where: { user_id: user.dataValues.id, type: 'FORGOT_PASS' } });
+            return responseService.respond(res, {}, 200, true, 'Password reset');
         } catch (error) {
-                        responseService.respond(res, error.data ? error.data : error, error.code && typeof error.code == 'number' ? error.code : 500, false, error.message ? error.message : 'Server error');
+            responseService.respond(res, error.data ? error.data : error, error.code && typeof error.code == 'number' ? error.code : 500, false, error.message ? error.message : 'Server error');
         }
     }
 }
